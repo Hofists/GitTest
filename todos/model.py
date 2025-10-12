634dce38786129e4faad016ab823a7cf2ef3f1bf
Фаттахов Раиль_2 <fattakhov.rail@inbox.ru>
2025-10-12 18:17:32 +0500

    Практика 4

---
+++ todos/model.py
@@ -0,0 +1,9 @@
+from pydantic import BaseModel
+class Todo(BaseModel):
+    id: int
+    item: str
+    class Config:
+        Schema_extra = {
+            "Example": {
+                "id": 1,
+                "item": "Example schema!"}}
